3e1f3b62982289389a1ff01346857d6e173113ff
cswdev <mbso520@gmail.com>
2024-06-13 15:13:35 +0900

    WebFlux 적용

--- Catalogs/src/main/java/egovframework/msa/sample/CatalogsApplication.java
+++ Catalogs/src/main/java/egovframework/msa/sample/CatalogsApplication.java
@@ -1,10 +1,12 @@
 package egovframework.msa.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("egovframework.*")
+@EnableFeignClients
 public class CatalogsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogsApplication.class);
--- Catalogs/src/main/java/egovframework/msa/sample/serviceImpl/CustomerApiServiceImpl.java
+++ Catalogs/src/main/java/egovframework/msa/sample/serviceImpl/CustomerApiServiceImpl.java
@@ -1,11 +1,27 @@
 package egovframework.msa.sample.serviceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import egovframework.msa.sample.service.CustomerApiService;
 @Service
 public class CustomerApiServiceImpl implements CustomerApiService {
+	private static final Logger log = LoggerFactory.getLogger(CustomerApiServiceImpl.class);
+	private final WebClient.Builder webClient;
+	public CustomerApiServiceImpl(WebClient.Builder webClient) {
+		this.webClient = webClient;
+	}
 	@Override
 	public String getCustomerDetail(String customerId) {
-		return customerId;
+		//return customerId;
+		return webClient.build().get()
+		.uri("http://localhost:8082/customers/" + customerId)
+		.retrieve()
+		.bodyToMono(String.class)
+		.flux()
+		.toStream()
+		.findFirst()
+		.orElse(null);
 	}
 }
